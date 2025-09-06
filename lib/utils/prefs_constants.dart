32cefcd635c6dbe06ebf4fdcc2a9e0f8d12d2309
Youssef Guba <youssefguba6@gmail.com>
2025-09-06 15:08:21 +0300

    Add additional preferences keys for user settings

--- lib/utils/prefs_constants.dart
+++ lib/utils/prefs_constants.dart
@@ -1,3 +1,11 @@
 class PrefsKeys {
   static const String todos = 'todos';
+  static const String themeMode = 'theme_mode';
+  static const String language = 'language';
+  static const String notificationsEnabled = 'notifications_enabled';
+  static const String fontSize = 'font_size';
+  static const String autoSync = 'auto_sync';
+  static const String backupFrequency = 'backup_frequency';
+  static const String lastBackupDate = 'last_backup_date';
+  static const String showCompletedTasks = 'show_completed_tasks';
 }
